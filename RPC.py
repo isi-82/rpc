7af57de9e36678b20a760e817c1951099d11e423
Isi 82 <81757363+isi-82@users.noreply.github.com>
2022-01-02 16:26:54 +0100

    Update RPC.py

--- RPC.py
+++ RPC.py
@@ -1,14 +1,18 @@
+# Import's
 from pypresence import Presence
 import time
 
-rpc_text = input("Your RPC Text : ")
-client_id = "[Your Client ID]"
+# Variabeln
+rpc_text = input("RPC Text: ")
+client_id = "client_id"
 RPC = Presence(client_id)
 
+# Start
 RPC.connect()
-RPC.update(state="RPC starting...")
-print("RPC starting...")
+RPC.update(state="RPC startet...")
+print("RPC startet!")
 
-while True: 
-    time.sleep(15) 
-    RPC.update(state=rpc_text)
+# Loop
+while True:
+  time.sleep(15)
+  RPC.update(state=rpc_text)
